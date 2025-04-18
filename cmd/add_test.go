a2e5c7c339c7a43346fa4ad42eef81e6727e752a
bllakcn <bllakcn35@gmail.com>
2025-04-18 10:49:38 +0200

    fix add_test

--- cmd/add_test.go
+++ cmd/add_test.go
@@ -10,52 +10,57 @@ import (
 
 func TestDeterminePathAndComponent(t *testing.T) {
 	tests := []struct {
-		configRouter         constants.RouterType
-		configLanguage       constants.LanguageType
-		configComponentStyle constants.ComponentStyleType
-		configSrcFolder      bool
-		inputPath            string
-		expectedTarget       string
-		expectedName         string
+		configRouter              constants.RouterType
+		configLanguage            constants.LanguageType
+		configComponentStyle      constants.ComponentStyleType
+		configPageComponentSuffix string
+		configSrcFolder           bool
+		inputPath                 string
+		expectedTarget            string
+		expectedName              string
 	}{
 		{
-			configRouter:         "app",
-			configLanguage:       "ts",
-			configComponentStyle: "function",
-			configSrcFolder:      false,
+			configRouter:              "app",
+			configLanguage:            "ts",
+			configComponentStyle:      "function",
+			configSrcFolder:           false,
+			configPageComponentSuffix: "page",
 
 			inputPath:      "dashboard",
 			expectedTarget: filepath.Join("app", "dashboard", "page.tsx"),
-			expectedName:   "AnalyticsPage",
+			expectedName:   "DashboardPage",
 		},
 		{
-			configRouter:         "pages",
-			configLanguage:       "ts",
-			configComponentStyle: "const",
-			configSrcFolder:      false,
+			configRouter:              "pages",
+			configLanguage:            "ts",
+			configComponentStyle:      "const",
+			configSrcFolder:           false,
+			configPageComponentSuffix: "page",
 
 			inputPath:      "auth/login",
 			expectedTarget: filepath.Join("pages", "auth", "login", "index.tsx"),
 			expectedName:   "LoginPage",
 		},
 		{
-			configRouter:         "app",
-			configLanguage:       "js",
-			configComponentStyle: "function",
-			configSrcFolder:      true,
+			configRouter:              "app",
+			configLanguage:            "js",
+			configComponentStyle:      "function",
+			configSrcFolder:           true,
+			configPageComponentSuffix: "component",
 
 			inputPath:      "products/details",
 			expectedTarget: filepath.Join("src", "app", "products", "details", "page.jsx"),
-			expectedName:   "DetailsPage",
+			expectedName:   "DetailsComponent",
 		},
 	}
 
 	for _, tt := range tests {
 		mockConfig := &constants.Config{
-			Router:         tt.configRouter,
-			Language:       tt.configLanguage,
-			ComponentStyle: tt.configComponentStyle,
-			SrcFolder:      tt.configSrcFolder,
+			Router:              tt.configRouter,
+			Language:            tt.configLanguage,
+			ComponentStyle:      tt.configComponentStyle,
+			SrcFolder:           tt.configSrcFolder,
+			PageComponentSuffix: tt.configPageComponentSuffix,
 		}
 		targetPath, componentName, err := determinePathAndComponent(tt.inputPath, mockConfig)
 		if err != nil {
